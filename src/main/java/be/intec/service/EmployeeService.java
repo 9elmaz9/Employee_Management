e9413c5c0047909c06b3136af9ca318b087469a8
Elmaz <kakbyelmaz@gmail.com>
2024-05-07 15:11:08 +0200

    add

---
+++ src/main/java/be/intec/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package be.intec.service;
+
+import be.intec.model.Employee;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+public interface EmployeeService {
+
+    List<Employee> getAllEmployees();
+    void saveEmployee(Employee employee);
+    Employee getEmployeeById(Long id);
+
+    void deleteEmployeeById(Long id);
+}
